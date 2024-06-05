83d837d84c00d626f64395ccc39f2a65d6a8ba28
lucarl07 <junitinho2007@gmail.com>
2024-06-05 19:04:17 -0300

    :sparkles: feat: Finish the profile's CardView expansion

--- app/src/main/java/com/example/arbeid/ui/fragments/UserProfile.kt
+++ app/src/main/java/com/example/arbeid/ui/fragments/UserProfile.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.example.arbeid.R
@@ -18,33 +19,30 @@ class UserProfile : Fragment() {
     private var _binding: FragmentUserProfileBinding? = null
     private val binding get() = _binding!!
 
-    // Declaring the later-initialized expandable CardViews:
-    private lateinit var expandableSkills: CardView
-    private lateinit var expandableExperience: CardView
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflating the layout:
         _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
-        expandableSkills = binding.userSkills
-        expandableExperience = binding.userExperience
 
-        binding.userSkills.setOnClickListener {
-            expandCardView(it, expandableSkills, binding.userSkillsList)
-        }
-        binding.userExperience.setOnClickListener {
-            expandCardView(it, expandableExperience, binding.userExperienceList)
+        binding.apply {
+            // Allowing for expansion in the CardViews:
+            userSkillsSelector.setOnClickListener {
+                expandCardView(userSkills, userSkillsList)
+            }
+            userExperienceSelector.setOnClickListener {
+                expandCardView(userExperience, userExperienceList)
+            }
         }
 
         return binding.root
     }
 
-    fun expandCardView(view: View, layout: CardView, accordion: View) {
-        val newView = if (accordion.visibility == View.GONE) {
-            View.VISIBLE
-        } else {
-            View.GONE
+    private fun expandCardView(layout: CardView, accordion: View) {
+        val newView = when (accordion.isVisible) {
+            false -> View.VISIBLE
+            true -> View.GONE
         }
 
         TransitionManager.beginDelayedTransition(layout, AutoTransition())
