3b1e482da9ddc9a08eab64fcd657cbfb1ed60360
lucarl07 <junitinho2007@gmail.com>
2024-06-06 18:31:27 -0300

    :broom: cleanup: Optimize imports at UserProfile.kt

--- app/src/main/java/com/example/arbeid/ui/fragments/UserProfile.kt
+++ app/src/main/java/com/example/arbeid/ui/fragments/UserProfile.kt
@@ -1,16 +1,14 @@
 package com.example.arbeid.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
-import com.example.arbeid.R
 import com.example.arbeid.databinding.FragmentUserProfileBinding
 
 class UserProfile : Fragment() {
