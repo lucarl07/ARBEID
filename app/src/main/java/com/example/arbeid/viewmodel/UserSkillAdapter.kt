ed6e208d70ccef1e1df66729c601f4d37fae2a66
lucarl07 <junitinho2007@gmail.com>
2024-06-06 18:02:09 -0300

    :sparkles: Finish coding the members of the UserSkillAdapter class

--- app/src/main/java/com/example/arbeid/viewmodel/UserSkillAdapter.kt
+++ app/src/main/java/com/example/arbeid/viewmodel/UserSkillAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.arbeid.viewmodel
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -11,18 +12,20 @@ class UserSkillAdapter(private val skillList: ArrayList<UserSkill>)
     : RecyclerView.Adapter<UserSkillAdapter.SkillViewHolder>() {
 
     class SkillViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
-         val textView: TextView = itemView.findViewById(R.id.skill_list_item)
+         val textView: TextView = itemView.findViewById(R.id.list_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SkillViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.profile_info_each_item, parent, false)
+        return SkillViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SkillViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val skill = skillList[position]
+        holder.textView.text = skill.name
+    }
+
+    override fun getItemCount(): Int {
+        return skillList.size
     }
 }
